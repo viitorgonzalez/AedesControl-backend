8ee873373e37d1d4751e2ea1061ee3eee9231009
viitorgonzalez <pastor123.vs@gmail.com>
2025-11-28 12:13:00 -0300

    feat(CookieService): foi adicionado debug logs nos métodos

--- src/main/java/com/aedescontrol/backend/service/CookieService.java
+++ src/main/java/com/aedescontrol/backend/service/CookieService.java
@@ -3,6 +3,8 @@ package com.aedescontrol.backend.service;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 
@@ -10,30 +12,46 @@ public class CookieService {
 
     private static final String TOKEN_COOKIE = "token";
 
+    private static final Logger log = LoggerFactory.getLogger(AddressService.class);
+
     public static void setCookie(HttpServletResponse response, String key, String value, int seconds) {
+        log.debug("Criando cookie: key='{}', maxAge={} segundos, httpOnly=true, path='/'", key, seconds);
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(seconds);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
         response.addCookie(cookie);
+        log.debug("Cookie {} adicionado no HTTP", key);
     }
 
     public static String getCookie(HttpServletRequest request, String key) {
-        if (request.getCookies() == null) return null;
+        log.debug("Buscando cookie: key={}", key);
+        if (request.getCookies() == null) {
+            log.warn("Nenhum cookie presente na requisição. Cookie key={} não encontrado.", key);
+            return null;
+        };
 
         return Arrays.stream(request.getCookies())
                 .filter(cookie -> key.equals(cookie.getName()))
-                .map(Cookie::getValue)
+                .map(cookie -> {
+                    log.debug("Cookie encontrado: key={}", key);
+                    return cookie.getValue();
+                })
                 .findFirst()
-                .orElse(null);
+                .orElseGet(() -> {
+                    log.warn("Cookie não encontrado: key={}", key);
+                    return null;
+                });
     }
 
     public static void clearTokenCookie(HttpServletResponse response) {
+        log.debug("Iniciando limpeza do cookie '{}'", TOKEN_COOKIE);
         Cookie cookie = new Cookie(TOKEN_COOKIE, null);
         cookie.setHttpOnly(true);
         cookie.setSecure(false);
         cookie.setPath("/");
         cookie.setMaxAge(0);
         response.addCookie(cookie);
+        log.debug("Cookie '{}' limpo com sucesso.", TOKEN_COOKIE);
     }
 }
